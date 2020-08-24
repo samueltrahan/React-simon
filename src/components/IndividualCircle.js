eadf96b3d482b889979b4526a06cb6d02836deba
Samuel Trahan <samtrahan91@gmail.com>
2020-08-24 17:58:07 -0500

    cirle buttons add

--- src/components/IndividualCircle.js
+++ src/components/IndividualCircle.js
@@ -1,7 +1,15 @@
 import React from "react";
+import './IndividualCircle.css';
 
 const IndividualCircle = () => {
-  return <div>Yellow Blue Green Red</div>;
+  return (
+    <div>
+      <button className="red-circle">Red</button>
+      <button className="blue-circle">Blue</button>
+      <button className="green-circle">Green</button>
+      <button className="yellow-circle">Yellow</button>
+    </div>
+  )
 };
 
 export default IndividualCircle;
