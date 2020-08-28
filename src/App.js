299148c6782fd7dcc859b459285dcda058ffc245
Samuel Trahan <samtrahan91@gmail.com>
2020-08-28 10:42:14 -0500

    dmade app a function component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import "./App.css";
 import IndividualCircle from "./components/IndividualCircle";
 import Score from "./components/Score";
 
-class App extends React.Component {
-  state = {
-    solutionSequence: [],
-    solutionSequenceIndex: 0,
-    roundBlinkCount: 0,
-    roundInterval: 0,
-    blinkDuration: 0,
-    blinkGapDuration: 0,
-    currentRound: 1,
-    currentScore: 0,
-    isWinner: false,
-  };
+export default function App() {
+    // solutionSequence: [],
+    // solutionSequenceIndex: 0,
+    // roundBlinkCount: 0,
+    // roundInterval: 0,
+    // blinkDuration: 0,
+    // blinkGapDuration: 0,
+    // currentRound: 1,
+    // currentScore: 0,
+    // isWinner: false,
 
-  render() {
+
     return (
       <div>
         <h1 className="simon-heading">Simon</h1>
@@ -33,6 +31,4 @@ class App extends React.Component {
       </div>
     );
   }
-}
 
-export default App;
