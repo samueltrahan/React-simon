c5b81c586408f7d98527261bd67d686cf7f407bf
Samuel Trahan <samtrahan91@gmail.com>
2020-08-31 12:39:10 -0500

    reset button set up

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import IndividualCircle from "./components/IndividualCircle";
 import Score from "./components/Score";
 
 export default function App() {
-    const [solutionSequence, setSolutionSequence] = useState([]);
-    const [solutionSequenceIndex, setSolutionSequenceIndex] = useState(0);
-    const [roundBlinkCount, setRoundBlinkCount] = useState(0);
-    const [roundInterval, setRoundInterval] = useState(0);
-    const [blinkDuration, setBlinkDuration] = useState(0); 
-    const [blinkGapDuration, setBlinkGapDuration] = useState(0); 
-    const [currentRound, setCurrentRound] = useState(1);
-    const [currentScore, setCurrentScore] = useState(0);
-    const [isWinner, setIsWinner] = useState(false);
+  const [solutionSequence, setSolutionSequence] = useState([]);
+  const [solutionSequenceIndex, setSolutionSequenceIndex] = useState(0);
+  const [roundBlinkCount, setRoundBlinkCount] = useState(0);
+  const [roundInterval, setRoundInterval] = useState(0);
+  const [blinkDuration, setBlinkDuration] = useState(1000);
+  const [blinkGapDuration, setBlinkGapDuration] = useState(1500);
+  const [currentRound, setCurrentRound] = useState(1);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [isWinner, setIsWinner] = useState(false);
 
-
-    function generateSolutionSequence() {
-      for(let i = 0; i < 20; i++) {
-        setSolutionSequence(Math.floor(Math.random() * 4) + 1)
-      }
+  function generateSolutionSequence() {
+    for (let i = 0; i < 20; i++) {
+      setSolutionSequence({
+        solutionSequence: Math.floor(Math.random() * 4) + 1,
+      });
     }
+  }
 
-    function startGameSequence() {
-      if(currentRound === 1) {
-        generateSolutionSequence()
-        console.log(solutionSequence)
-      }
+  function startGameSequence() {
+    if (currentRound === 1) {
+      generateSolutionSequence();
     }
+  }
 
-    return (
-      <div>
-        <h1 className="simon-heading">Simon</h1>
-        <div className="simon-btn-board">
+  function resetGame() {
+    setSolutionSequence({ solutionSequence: [] });
+    setSolutionSequenceIndex({ solutionSequenceIndex: 0 });
+    setRoundBlinkCount({ roundBlinkCount: 0 });
+    setIsWinner({ isWinner: false });
+    setCurrentRound({ currentScore: 1 });
+    setCurrentScore({ currentScore: 0 });
+    clearInterval(roundInterval);
+  }
+
+  return (
+    <div>
+      <h1 className="simon-heading">Simon</h1>
+      <div className="simon-btn-board">
         <IndividualCircle color={"red"} />
 
         <IndividualCircle color={"blue"} />
@@ -39,12 +49,11 @@ export default function App() {
         <IndividualCircle color={"green"} />
 
         <IndividualCircle color={"yellow"} />
-        </div>
-        <button onClick={() => startGameSequence()}>Start Game</button>
-        <button >Reset</button>
-
-        <Score />
       </div>
-    );
-  }
+      <button onClick={() => startGameSequence()}>Start Game</button>
+      <button onClick={() => resetGame()}>Reset</button>
 
+      <Score />
+    </div>
+  );
+}
